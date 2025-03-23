3fce95458a0c025ce76e9aa83c7a4a5a9fe41a64
ahmad-1212 <ahmadali.swat333@gmail.com>
2025-03-23 11:08:15 +0500

    remove name

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h5>Ahmad ali</h5>
+      <h5></h5>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
